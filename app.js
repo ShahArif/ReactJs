6ea280ce120c1eb4a737b3f630ff67dbde69de78
ariflateefshah <arif@ideas2it.com>
2019-08-15 15:35:26 +0530

    Feature: Hello world | Creating Div with child elements

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+
 var h1 =  React.createElement('h1',null ,'Hello World from App file');
+var p =  React.createElement('p', null ,'This is the description area which belongs to the div')
 var myApp = document.getElementById('myApp');
 ReactDOM.render(
-    h1,myApp 
+    React.createElement('Div' , null , h1 ,p),
+    myApp 
  );
