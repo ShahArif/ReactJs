747458e618a2bad3224cffdac50ecddfd4b8238a
ariflateefshah <arif@ideas2it.com>
2019-08-15 17:55:08 +0530

    Feature: Passing elements to custom component

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var h1 = React.createElement('h1',null , 'Hello world from Custom Component');
+var p = React.createElement('p', null , 'Hello world from Custom Component');
+
 class CustomComponent extends React.Component{
     render(){
-        return React.createElement('h1',null , 'Hello world from Custom Component');
+        return (
+            React.createElement('div',null ,h1,p)
+        );
     }
 }
 
