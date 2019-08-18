5a9424d105206d3336ebc57bcc2569d300ea569e
ariflateefshah <arif@ideas2it.com>
2019-08-18 16:50:14 +0530

    Feature:Handling radio button and select in reactjs

--- src/Form.js
+++ src/Form.js
@@ -6,10 +6,25 @@ class Form extends Component{
         super(props);
         this.state = {
             username:'',
-            email:''
+            email:'',
+            technologies : [
+                   { name:'Vuejs',id:0},
+                   { name:'ReactJs',id:1},
+                   { name:'NodeJs',id:2},
+                    
+                    
+            ],
+            genderGroup:{
+                male:false,
+                female:true
+            }
         };
+
+      
+
         this.changeName = this.changeName.bind(this);
         this.changeEmail = this.changeEmail.bind(this);
+        this.radioHandler = this.radioHandler.bind(this);
     }
 
     changeName(ev){
@@ -23,6 +38,19 @@ class Form extends Component{
             email:e.target.value
         })
     }
+    radioHandler(event){
+        console.log(event.target.value)
+        let agenderGroup = this.state.genderGroup;
+        for(var key in agenderGroup){
+            agenderGroup[key]= false
+        }
+        agenderGroup[event.target.value] = event.target.checked
+        this.setState({
+            genderGroup:agenderGroup
+        });
+
+    }
+
     render(){
         return(
             <div className="container">
@@ -32,6 +60,21 @@ class Form extends Component{
                         <form>
                             <input type="text" name="username" className="form-control" value={this.state.username} onChange={this.changeName}/>
                             <input type="email" name="email" className="form-control" value={this.state.email} onChange={this.changeEmail}/>
+                          <div className="col-md-12">
+                            <select >
+                                    <option>Select Technology</option>
+                                {                               
+                                    this.state.technologies.map((tech) =>
+                                        <option key={tech.id}>{tech.name}</option>
+                                    )   
+                                }
+                                </select>
+                          </div>
+                          <div className="col-md-12">
+                              <input type="radio" value='male' checked={this.state.genderGroup['male']}  name="gender" onChange={this.radioHandler}/>
+                              <input type="radio" value='female' name="gender"  checked={this.state.genderGroup['female']} onChange={this.radioHandler}/>
+                          </div>
+
                         </form>
                     </div>
                     <div className="col-md-3">
